64e5a610aa470b7d43ac0f2afa3650d81b9fb6fc
Sherry Wu <Sherry_wu@epam.com>
2019-11-22 16:29:43 +0800

    full-stack-weather-forecast

---
+++ week-05/day-04/weather-forecast-full stack/test.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+app.use('/static', express.static('static'));
+
+app.set('view engine', 'ejs');
+
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'weather'
+})
+
+
+app.get('/', (req, res) => {
+  const { cityName, location, temp, icon } = req.body;
+  const queryString = `WITH summary AS (
+    SELECT cityID, temp, icon, row_number() OVER (partition by cityID order by weatherID) as ranks
+    From weather)
+  SELECT cityName, location, temp, icon
+    FROM city, summary
+    where city.cityID = summary.cityID and summary.ranks = 1`;
+
+  conn.query(queryString, [cityName, location, temp, icon], (err, result) => {
+    if (err) {
+      console.log('cannot query', err);
+      return res.sendStatus(500);
+    }
+    res.render('dashboard', { result });
+  })
+});
+
+app.get('/cities/:id', (req, res) => {
+  let id = req.params.id;
+  const { cityName, temp, icon, message } = req.body
+  const queryString = `SELECT cityName, temp, icon, message 
+  FROM weather, city
+  WHERE weather.cityID = city.cityID AND city.cityID = ${id};`
+  conn.query(queryString, [cityName, temp, icon, message], (err, result) => {
+    if (err) {
+      console.log('cannot query', err);
+      return res.sendStatus(500);
+    }
+    res.render('detailedView', { result });
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
